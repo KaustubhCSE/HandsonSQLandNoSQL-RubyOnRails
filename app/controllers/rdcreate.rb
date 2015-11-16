491b6156b1b2ac9133dc91ec21e2e808e0f4eeb9
KaustubhCSE <kaustubh.mohgaonkar@mavs.uta.edu>
2015-11-15 18:26:34 -0600

    3 files

--- app/controllers/rdcreate.rb
+++ app/controllers/rdcreate.rb
@@ -1,8 +1,8 @@
 require "redis"
 
-# redis = Redis.new(url: ENV["REDIS_URL"])
+redis = Redis.new(url: ENV["REDIS_URL"])
 
-redis = Redis.new(:host => "10.0.1.1", :port => 6380, :db => 15)
+# redis = Redis.new(:host => "10.0.1.1", :port => 6380, :db => 15)
 
 redis.set('foo', 'bar');
 value = redis.get('foo');
